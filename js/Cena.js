38b1f2dccf1b20e1255181a755d4e5a3838e4e4c
julianapsilva <juliana.silva@estudante.ufjf.br>
2022-01-13 19:47:15 -0300

    cria e usa uma classe de cena

---
+++ js/Cena.js
@@ -0,0 +1,11 @@
+export default class Cena {
+    // responsavel por desenhar elementos na tela em uma animação
+    constructor(canvas) {
+        this.canvas = canvas
+        this.ctx = canvas.getContext("2d")
+    }
+    desenhar() {
+        this.fillStyle = "grey"
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+}
--- js/Main.js
+++ js/Main.js
@@ -1,2 +1,5 @@
-const canvas = document.querySelectorAll("canvas")
+import Cena from "./Cena.js"
+const canvas = document.querySelector("canvas")
 console.log(canvas)
+const cena1 = new Cena(canvas)
+cena1.desenhar()
