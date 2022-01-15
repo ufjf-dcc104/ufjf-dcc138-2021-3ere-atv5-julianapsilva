1839d15db608c55e1ce983b1ef5268b1df873526
julianapsilva <juliana.silva@estudante.ufjf.br>
2022-01-15 19:35:32 -0300

    refatora load e uso de imagem para o assetManager

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -2,10 +2,24 @@ export default class AssetManager{
     constructor() {
         this.aCarregar = 0
         this.carregadas = 0
+        this.images = new Map()
     }
+
+    carregaImagem(chave, source) {
+        const img1 = new Image()
+        img1.src = source
+        this.images.set(chave, img1)
+    }
+
+    img(chave) {
+        return this.images.get(chave)
+    }
+
+
     progresso() {
         if (this.aCarregar > 0)
             return `${(this.carregadas / this.aCarregar * 100).toFixed(2)}%`
         return 'Nada a carregar'
     }
+
 }
