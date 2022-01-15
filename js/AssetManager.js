9b4c4187fca15bf54e8afe1177d39c006846a322
julianapsilva <juliana.silva@estudante.ufjf.br>
2022-01-15 19:42:36 -0300

    adiciona metodo acabou

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -7,19 +7,27 @@ export default class AssetManager{
 
     carregaImagem(chave, source) {
         const img1 = new Image()
+        img1.addEventListener('load', () => {
+            console.log(`Imagem ${this.carregadas}/${this.aCarregar} carregada`);
+            this.carregadas++
+        })
         img1.src = source
         this.images.set(chave, img1)
+        this.aCarregar++
     }
 
     img(chave) {
         return this.images.get(chave)
     }
 
-
     progresso() {
         if (this.aCarregar > 0)
             return `${(this.carregadas / this.aCarregar * 100).toFixed(2)}%`
         return 'Nada a carregar'
+    }
+
+    acabou() {
+        return this.carregadas === this.aCarregar
     }
 
 }
