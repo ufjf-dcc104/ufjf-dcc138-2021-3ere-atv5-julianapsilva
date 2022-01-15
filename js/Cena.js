07c30c51c289d0fd418cb53d4eeb42f66c97f201
julianapsilva <juliana.silva@estudante.ufjf.br>
2022-01-15 19:45:54 -0300

    adiciona restrição por assets na Cena

--- js/Cena.js
+++ js/Cena.js
@@ -13,9 +13,11 @@ export default class Cena {
     desenhar() {
         this.ctx.fillStyle = "grey"
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-        for (let s = 0; s < this.sprites.length; s++) {
-            const sprite = this.sprites[s];
-            sprite.desenhar(this.ctx)
+        if (this.assets.acabou()) {
+            for (let s = 0; s < this.sprites.length; s++) {
+                const sprite = this.sprites[s];
+                sprite.desenhar(this.ctx)
+            }
         }
         this.ctx.fillStyle = 'yellow'
         this.ctx.fillText(this.assets?.progresso(), 10, 20)
@@ -26,8 +28,10 @@ export default class Cena {
     }
 
     passo(dt) {
-        for (const sprite of this.sprites) {
-            sprite.passo(dt)
+        if (this.assets.acabou()) {
+            for (const sprite of this.sprites) {
+                sprite.passo(dt)
+            } 
         }
     }
 
