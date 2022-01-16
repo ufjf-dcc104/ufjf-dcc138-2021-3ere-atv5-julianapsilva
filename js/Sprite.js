d01f9201527c39661a3fda7254032589ad5d1f17
julianapsilva <juliana.silva@estudante.ufjf.br>
2022-01-16 11:22:53 -0300

    adiciona restricao a esquerda

--- js/Sprite.js
+++ js/Sprite.js
@@ -38,7 +38,13 @@ export default class Sprite {
         )
     }
     aplicaRestricoes(dt) {
+        this.aplicaRestricoesDireita(dt)
+        this.aplicaRestricoesEsquerda(dt)
+    }
+
+    aplicaRestricoesDireita(dt) {
         const SIZE = this.cena.mapa.SIZE
+
         if (this.vx > 0) {
             const pmx = this.mx + 1
             const pmy = this.my
@@ -58,7 +64,29 @@ export default class Sprite {
                 }
             }
         }
+    }
+    aplicaRestricoesEsquerda(dt) {
+        const SIZE = this.cena.mapa.SIZE
 
+        if (this.vx < 0) {
+            const pmx = this.mx - 1
+            const pmy = this.my
+
+            if (this.cena.mapa.tiles[pmy][pmx] != 0) {
+                const tile = {
+                    x: pmx * SIZE + SIZE / 2,
+                    y: pmy * SIZE + SIZE / 2,
+                    w: SIZE,
+                    h: SIZE
+                }
+                this.cena.ctx.strokeStyle = 'white'
+                this.cena.ctx.strokeRect(tile.x - SIZE / 2, tile.y - SIZE / 2, SIZE, SIZE)
+                if (this.colidiuCom(tile)) {
+                    this.vx = 0
+                    this.x = tile.x + tile.w / 2 + this.w / 2 + 1
+                }
+            }
+        }
     }
 
 }
