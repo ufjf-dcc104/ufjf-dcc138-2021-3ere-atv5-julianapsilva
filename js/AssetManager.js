91056e48b8cd737b822bdea77102aff0b64a0b2c
julianapsilva <juliana.silva@estudante.ufjf.br>
2022-01-17 10:21:19 -0300

    corrige evento de carregamento de audio

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -19,7 +19,7 @@ export default class AssetManager{
 
     carregaAudio(chave, source) {
         const audio = new Audio()
-        audio.addEventListener('canplay', () => {
+        audio.addEventListener('loadeddata', () => {
             console.log(`Audio ${this.carregadas}/${this.aCarregar} carregado`);
             this.carregadas++
         })
